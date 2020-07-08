6a21b762a450b5ca2d9115fad6ba5b6369cb6de7
CubeDr <spaceship00@naver.com>
2020-07-08 21:23:10 +0900

    RFT: Integrate proto compile scripts

--- compile-java.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-protoc --proto_path=src/main/resources/proto --java_out=src/main/java src/main/resources/proto/%1.proto
--- compile-js.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-protoc --proto_path=src/main/resources/proto --js_out=import_style=commonjs,binary:src/main/resources/js-modules src/main/resources/proto/%1.proto
