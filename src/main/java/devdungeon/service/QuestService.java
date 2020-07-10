eeaa1d5edb874c0ccd74b818fd77e1fec233981b
2014004166 <2014004166@hanyang.ac.kr>
2020-07-10 22:22:16 +0900

    commit Home

---
+++ src/main/java/devdungeon/service/QuestService.java
@@ -0,0 +1,13 @@
+package devdungeon.service;
+
+import devdungeon.domain.QuestVO;
+
+import java.util.List;
+
+public interface QuestService {
+    List<QuestVO> getAll();
+
+    QuestVO getOne(int id);
+
+    List<QuestVO> getRecent(int amount);
+}
