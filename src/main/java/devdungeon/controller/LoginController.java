4daf12dbddb0aefdfc69be96db232db136f23aec
kbcbang <kbcbang@gmail.com>
2020-07-13 21:24:40 +0900

    LoginController login success redirect

--- src/main/java/devdungeon/controller/LoginController.java
+++ src/main/java/devdungeon/controller/LoginController.java
@@ -4,15 +4,17 @@ package devdungeon.controller;
 import devdungeon.domain.UserVO;
 import devdungeon.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @Controller
 @RequiredArgsConstructor
-public class LoginAPI {
+public class LoginController {
 
     private final UserService userService;
     @GetMapping("/login")
@@ -21,9 +23,10 @@ public class LoginAPI {
     }
     @PostMapping("/login")
     public String postLogin(@RequestBody UserVO user){
-		if(userService.findUser(user.getId(),user.getPassword()))
-			return "loginsuccess";
-		return "loginfail";
+		if(userService.findUser(user.getId(),user.getPassword())) {
+            return "redirect:/";
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Wrong user id or password");
     	
     }
 }
