db9566cfdfa823d2e6d58d7d2d1e9bfd15ff15a5
CubeDr <spaceship00@naver.com>
2020-07-15 21:42:44 +0900

    ADD: Add quest list template

---
+++ src/main/java/devdungeon/test/HyunIController.java
@@ -0,0 +1,23 @@
+package devdungeon.test;
+
+import devdungeon.domain.QuestVO;
+import devdungeon.service.QuestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class HyunIController {
+    private final QuestService questService;
+
+    @GetMapping("/hyuni")
+    public String getList(Model model) {
+        List<QuestVO> quests = questService.getAll();
+        model.addAttribute("questList", quests);
+        return "list";
+    }
+}
