7693aabc8d1d0d915ac8dbbf735267dcb5cc850a
2014004166 <2014004166@hanyang.ac.kr>
2020-07-15 14:55:14 +0900

    ADD : write와 edit의 경우 인증.

--- src/main/java/devdungeon/controller/QuestController.java
+++ src/main/java/devdungeon/controller/QuestController.java
@@ -1,15 +1,17 @@
 package devdungeon.controller;
 
+import devdungeon.annotation.AuthAnnotation;
+import devdungeon.annotation.CertifyAnnotation;
 import devdungeon.domain.QuestVO;
+import devdungeon.domain.UserVO;
 import devdungeon.service.QuestService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class QuestController {
 
     private final QuestService questService;
+    private final HttpSession session;
 
     @GetMapping("/list")
     public void getQuestList(Model model) {
@@ -24,25 +27,29 @@ public class QuestController {
     }
 
     @GetMapping("/write")
+    @CertifyAnnotation
     public String getQuestWrite() {
         return "questWrite";
     }
 
     @PostMapping("/write")
+    @CertifyAnnotation
     public String postQuestWrite(@RequestBody QuestVO questVO) {
-        questVO.setAuthor("asdf");
+        UserVO userVO = (UserVO) session.getAttribute("user");
+        questVO.setAuthor(userVO.getId());
         questService.addQuest(questVO);
         return "redirect:/quest/list";
     }
 
     @GetMapping("/edit")
+    @AuthAnnotation
     public String getQuestEdit(){
         return "questEdit";
     }
 
     @PutMapping("/edit")
+    @AuthAnnotation
     public String putQuestEdit(@RequestBody QuestVO questVO){
-        //login된 유저와 questVO author와 같은지 인증 절차 필요
         questService.editQuest(questVO);
         return "questEdit";
     }
