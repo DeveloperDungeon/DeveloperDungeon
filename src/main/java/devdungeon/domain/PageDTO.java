937ec3d3389928e7f539c7b682220cea60db7954
2014004166 <2014004166@hanyang.ac.kr>
2020-07-16 10:50:37 +0900

    FIX : 기존의 PageCriteria라는 domain과 합침.

--- src/main/java/devdungeon/domain/PageDTO.java
+++ src/main/java/devdungeon/domain/PageDTO.java
@@ -4,28 +4,32 @@ import lombok.Data;
 
 @Data
 public class PageDTO {
+    private int limit;
+    private int offset;
+    private int currentPage;
 
     private int total;
     private int minPage;
     private int maxPage;
     private boolean prev, next;
 
-    private PageCriteria pageCriteria;
+    public PageDTO(int currentPage, int total) {
+        this.currentPage = currentPage;
+        this.limit = 10;
+        this.offset = this.limit * (this.currentPage - 1);
 
-    public PageDTO(PageCriteria pageCriteria, int total) {
         this.total = total;
-        this.pageCriteria = pageCriteria;
 
-
-        this.maxPage = (int) Math.ceil(pageCriteria.getCurrentPage() / 10.0) * 10;
+        this.maxPage = (int) Math.ceil(currentPage / 10.0) * 10;
         this.minPage = maxPage - 9;
 
-        int realEndPage = (int) Math.ceil((total * 1.0) / pageCriteria.getLimit());
+        int realEndPage = (int) Math.ceil((total * 1.0) / limit);
 
         this.maxPage = maxPage < realEndPage ? this.maxPage : realEndPage;
 
-        this.prev = this.pageCriteria.getCurrentPage() > 10;
+        this.prev = this.currentPage > 10;
         this.next = this.maxPage != realEndPage;
     }
+
 
 }
