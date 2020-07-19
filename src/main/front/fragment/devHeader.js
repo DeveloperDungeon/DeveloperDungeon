a99d338c94314826070e40f0f9dccde42c8c2c4e
CubeDr <spaceship00@naver.com>
2020-07-19 11:27:05 +0900

    FIX: Fix header redirection

--- src/main/front/fragment/devHeader.js
+++ src/main/front/fragment/devHeader.js
@@ -1,15 +1,5 @@
-import {redirect} from "../common/utils";
-
 window.addEventListener('load', () => {
-    document.querySelector('#main').onclick = () => {
-        redirect('/');
-    };
-
     const menuItems = Array.from(document.getElementsByClassName('header-menu'));
-    menuItems.forEach(i => {
-        i.onclick = () => redirect('/hyuni');
-    });
-
     const indicator = document.getElementById('header-indicator');
     menuItems.map(i => i.getElementsByTagName('a')[0]).forEach(a => {
         a.addEventListener('mouseenter', () => {
