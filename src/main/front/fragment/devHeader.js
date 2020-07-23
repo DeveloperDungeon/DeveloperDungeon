f852c8904b61602b0541bd8fc7d6d76eb5fed023
CubeDr <spaceship00@naver.com>
2020-07-23 21:00:48 +0900

    ADD: Header indicator returns to original position on mouse leave

--- src/main/front/fragment/devHeader.js
+++ src/main/front/fragment/devHeader.js
@@ -2,17 +2,23 @@ window.addEventListener('load', () => {
     // TODO parse url and assign selected menu index
     const selectedIndex = -1;
 
-    const menuItems = Array.from(document.getElementsByClassName('header-menu'));
+    const menuItems
+        = /** @type Array<HTMLElement> */ Array.from(document.getElementsByClassName('header-menu'));
     const indicator = document.getElementById('header-indicator');
-    indicator.style.visibility = 'hidden';
+    indicator.style.visibility = 'visible';
 
     const selectedMenuItem = menuItems[selectedIndex];
-    if (selectedMenuItem != null) {
-        matchIndicator(indicator, selectedMenuItem.getElementsByTagName('a')[0]);
-    }
+    matchIndicator(indicator, selectedMenuItem);
 
-    menuItems.map(i => i.getElementsByTagName('a')[0]).forEach(a => {
-        a.addEventListener('mouseenter', () => matchIndicator(indicator, a));
+    menuItems.forEach(menuItem => {
+        menuItem.addEventListener('mouseenter', () => matchIndicator(indicator, menuItem));
+    });
+
+    const menuArea = document.getElementById('header-navigator');
+    menuArea.addEventListener('mouseleave', () => {
+        console.log('leave');
+        const original = menuItems[selectedIndex];
+        matchIndicator(indicator, original);
     });
 });
 
@@ -22,17 +28,27 @@ window.addEventListener('load', () => {
  * @param targetElement {!HTMLElement}
  */
 function matchIndicator(indicator, targetElement) {
-    const width = targetElement.offsetWidth;
-    const left = targetElement.getBoundingClientRect().left;
-    indicator.style.width = `${width}px`;
-    indicator.style.left = `${left}px`;
-
-    if (indicator.style.visibility === 'hidden') {
-        const originalTop = indicator.style.top;
-        indicator.style.top = '20px';
+    if (targetElement == null) {
+        // 사라질 때
+        indicator.style.top = TOP_HIDDEN;
         indicator.offsetHeight;
-        indicator.classList.toggle('header-indicator-smooth');
-        indicator.style.top = originalTop;
-        indicator.style.visibility = 'visible';
+    } else {
+        const anchor = targetElement.getElementsByTagName('a')[0];
+        const width = anchor.offsetWidth;
+        const left = anchor.getBoundingClientRect().left;
+        indicator.style.width = `${width}px`;
+        indicator.style.left = `${left}px`;
+
+        if (indicator.style.top === TOP_HIDDEN) {
+            console.log(indicator, targetElement);
+            // 나올 때
+            indicator.classList.remove('header-indicator-smooth');
+            indicator.offsetHeight;
+            indicator.classList.add('header-indicator-smooth');
+            indicator.style.top = TOP_VISIBLE;
+        }
     }
 }
+
+const TOP_HIDDEN = '20px';
+const TOP_VISIBLE = '45px';
