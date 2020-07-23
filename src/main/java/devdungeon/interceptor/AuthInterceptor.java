65cb22b63882a312a9dacf76b1615b7da87e08a7
2014004166 <2014004166@hanyang.ac.kr>
2020-07-23 13:13:56 +0900

    ADD: 해당 url 접근권한 제어 인터셉터

---
+++ src/main/java/devdungeon/interceptor/AuthInterceptor.java
@@ -0,0 +1,51 @@
+package devdungeon.interceptor;
+
+import devdungeon.annotation.AuthAnnotation;
+import devdungeon.service.QuestService;
+import devdungeon.service.ReplyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class AuthInterceptor extends HandlerInterceptorAdapter {
+
+    @Resource
+    private QuestService questService;
+
+    @Resource
+    private ReplyService replyService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+
+            HandlerMethod method = (HandlerMethod) handler;
+
+            if (method.hasMethodAnnotation(AuthAnnotation.class)) {
+                String user = (String) request.getSession().getAttribute("user");
+                String[] str = request.getServletPath().split("/");
+
+                int Id = Integer.parseInt(str[str.length - 1]);
+
+                if (str[1].equals("quest")) {
+                    if (!user.equals(questService.getOne(Id).getAuthor())) {
+                        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,
+                                "you do not have auth to access this page");
+                    }
+                } else if (str[1].equals("reply")) {
+                    if (!user.equals(replyService.getReply(Id).getAuthor())) {
+                        throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,
+                                "you do not have auth to access this page");
+                    }
+                }
+            }
+        }
+        return super.preHandle(request, response, handler);
+    }
+}
