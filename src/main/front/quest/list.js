ae4dc470019346c59b1454deff4661ed29048aa7
이영차 <kares64@gmail.com>
2020-07-24 15:17:39 +0900

    ADD : Add link connected quest list and view

--- src/main/front/quest/list.js
+++
@@ -1,8 +0,0 @@
-import {redirect} from "../common/utils";
-console.log('jsfile');
-window.addEventListener('load', () => {
-
-    document.getElementsByClassName("quest-card").onclick = () => {
-        redirect('/quest/' + id);
-    }
-});
--- src/main/front/quest/view.js
+++ src/main/front/quest/view.js
@@ -1,12 +1,15 @@
-import {redirect} from "../common/utils";
+import {request, RequestMethod} from "../common/request";
 
 window.addEventListener('load', () => {
-    const id = document.getElementById('quest_id').value;
-    document.getElementById('bntEdit').onclick = () => {
-        redirect('/quest/edit/' + id);
-    }
-
-    document.getElementById('bntRemove').onclick = () => {
-        redirect('quest/remove/' + id)
+    const bntDelete = document.getElementById('bntRemove');
+    bntDelete.onclick = () => {
+        const url = require('url');
+        const currentURL = url.parse(window.location.href);
+        const path = currentURL.pathname.split('/');
+        const id = path[2];
+        request('/delete/' + id, {
+            method:RequestMethod.DELETE
+        })
+    //    request 결과 받아서 성공, 실패 처리
     }
 });
