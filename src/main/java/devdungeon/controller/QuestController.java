dfd1f56f3bb7d3f5269ce5009242c2b91f692e0a
kbcbang <kbcbang@gmail.com>
2020-07-24 10:51:57 +0900

    FIX: Quest edit put->post

--- src/main/java/devdungeon/controller/QuestController.java
+++ src/main/java/devdungeon/controller/QuestController.java
@@ -6,11 +6,9 @@ import devdungeon.domain.PageVO;
 import devdungeon.domain.QuestVO;
 import devdungeon.service.QuestService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
@@ -62,12 +60,12 @@ public class QuestController {
         return "quest/write";
     }
 
-    @PutMapping("/edit/{id}")
+    @PostMapping("/edit/{id}")
     @AuthAnnotation
     public String putQuestEdit(@PathVariable("id") Integer id, @RequestBody QuestVO questVO) {
         questVO.setId(id);
         questService.editQuest(questVO);
-        return "redirect:/quest/"+id;
+        return "redirect:/quest/" + id;
     }
 
     @DeleteMapping("/remove/{id}")
