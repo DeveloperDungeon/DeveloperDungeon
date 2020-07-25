35b2a8194bf0994b9ca3c26a7bc945109e186aea
2014004166 <2014004166@hanyang.ac.kr>
2020-07-25 18:22:05 +0900

    RFT: DI방식으로 변경

--- src/main/java/devdungeon/config/InterceptorConfig.java
+++ src/main/java/devdungeon/config/InterceptorConfig.java
@@ -2,21 +2,20 @@ package devdungeon.config;
 
 import devdungeon.interceptor.AuthInterceptor;
 import devdungeon.interceptor.LoginInterceptor;
-import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class InterceptorConfig implements WebMvcConfigurer {
+    private final LoginInterceptor loginInterceptor;
+    private final AuthInterceptor authInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor());
-        registry.addInterceptor(authInterceptor());
-    }
-
-    @Bean
-    public AuthInterceptor authInterceptor() {
-        return new AuthInterceptor();
+        registry.addInterceptor(loginInterceptor);
+        registry.addInterceptor(authInterceptor);
     }
 }
