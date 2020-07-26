ac1790bc5131c834bc8c2fa048e5fd252051b7d1
chlrlrhs95 <2014004166@hanyang.ac.kr>
2020-07-26 13:03:46 +0900

    ADD: 요청에 대한 커스텀 응답코드 및 바디추가.
    요청에따른 Redirect에 대한 기본 폼을 구성하는 도메인

---
+++ src/main/java/devdungeon/templete/Response.java
@@ -0,0 +1,25 @@
+package devdungeon.templete;
+
+import lombok.Data;
+
+@Data
+public class Response<T> {
+    public static final class Code {
+        public static final int OK = 200;
+        public static final int REDIRECT = 300;
+        public static final int BAD_REQUEST = 400;
+        public static final int UNAUTHORIZED = 401;
+        public static final int FORBIDDEN = 403;
+        public static final int NOT_FOUND = 404;
+        public static final int METHOD_NOT_ALLOWED = 405;
+        public static final int INTERNAL_SERVER_ERROR = 500;
+    }
+
+    private int code;
+    private T body;
+
+    public Response(int code, T body) {
+        this.code = code;
+        this.body = body;
+    }
+}
