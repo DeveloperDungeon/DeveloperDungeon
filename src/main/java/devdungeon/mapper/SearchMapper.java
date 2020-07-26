373a84f5475853230a886bbba83cbc30be43c1a0
kbcbang <kbcbang@gmail.com>
2020-07-26 11:54:11 +0900

    FIX: Search sql 쿼리 수정

--- src/main/java/devdungeon/mapper/SearchMapper.java
+++ src/main/java/devdungeon/mapper/SearchMapper.java
@@ -8,15 +8,15 @@ import java.util.List;
 
 @Mapper
 public interface SearchMapper {
-    @Select("SELECT * FROM quest WHERE author LIKE %{text}%")
+    @Select("SELECT * FROM quest WHERE author LIKE %#{text}%")
     List<QuestVO> searchByAuthor(String text);
 
-    @Select("SELECT * FROM quest WHERE content LIKE %{text}%")
+    @Select("SELECT * FROM quest WHERE content LIKE %#{text}%")
     List<QuestVO> searchByContent(String text);
 
-    @Select("SELECT * FROM quest WHERE title LIKE %{text}%")
+    @Select("SELECT * FROM quest WHERE title LIKE %#{text}%")
     List<QuestVO> searchByTitle(String text);
 
-    @Select("SELECT * FROM quest WHERE (title LIKE %{text}% OR content LIKE %{text}%)")
+    @Select("SELECT * FROM quest WHERE (title LIKE %#{text}% OR content LIKE %{text}%)")
     List<QuestVO> searchByTitleContent(String text);
 }
