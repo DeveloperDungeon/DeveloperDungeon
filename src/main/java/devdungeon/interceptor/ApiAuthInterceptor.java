8c5c0bbb3a5f4ed032ccf2b279e40a627a0c5fd2
chlrlrhs95 <2014004166@hanyang.ac.kr>
2020-07-29 21:41:56 +0900

    ADD: API서버를 위한 인터셉터 추가.

---
+++ src/main/java/devdungeon/interceptor/ApiAuthInterceptor.java
@@ -0,0 +1,49 @@
+package devdungeon.interceptor;
+
+import devdungeon.annotation.ApiAuthAnnotation;
+import devdungeon.service.CommentService;
+import devdungeon.service.QuestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApiAuthInterceptor extends HandlerInterceptorAdapter {
+    private final QuestService questService;
+    private final CommentService commentService;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HandlerMethod method = (HandlerMethod) handler;
+
+        if (method.hasMethodAnnotation(ApiAuthAnnotation.class)) {
+            String user = (String) request.getSession().getAttribute("user");
+            String[] str = request.getServletPath().split("/");
+
+            int id = Integer.parseInt(str[str.length - 1]);
+
+            if (str[1].equals("quest")) {
+                if (!user.equals(questService.getOne(id).getAuthor())) {
+                    throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+                            "you do not have auth to access this page");
+                }
+            }
+
+            else if (str[1].equals("comment")) {
+                if (!user.equals(commentService.getReply(id).getAuthor())) {
+                    throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+                            "you do not have auth to access this page");
+                }
+            }
+        }
+
+        return super.preHandle(request, response, handler);
+    }
+}
---
+++ src/main/java/devdungeon/interceptor/ApiLoginInterceptor.java
@@ -0,0 +1,28 @@
+package devdungeon.interceptor;
+
+import devdungeon.annotation.ApiCertifyAnnotation;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+public class ApiLoginInterceptor extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod method = (HandlerMethod) handler;
+            String user = (String) request.getSession().getAttribute("user");
+            if (method.hasMethodAnnotation(ApiCertifyAnnotation.class) && user == null) {
+
+                String redUrl = "/login";
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "/login");
+            }
+        }
+        return super.preHandle(request, response, handler);
+    }
+}
