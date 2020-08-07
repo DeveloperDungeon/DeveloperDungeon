481c3c33dfdf060ba0b0e03e50b266ae96de69f6
이영차 <kares64@gmail.com>
2020-08-07 14:09:09 +0900

    ADD: 댓글 수정기능 추가

--- src/main/front/elements/comment.js
+++ src/main/front/elements/comment.js
@@ -16,6 +16,7 @@ export class Comment extends HTMLElement {
         /** @type ?number */
         this.regTime = null;
 
+
         /** @type ?HTMLElement */
         this.authorDiv = null;
         /** @type ?HTMLElement */
@@ -26,6 +27,12 @@ export class Comment extends HTMLElement {
         this.bntModify = null;
         /** @type ?HTMLElement */
         this.bntDelete = null;
+        /** @type ?HTMLElement */
+        this.bntConfirm = null;
+        /** @type ?HTMLElement */
+        this.bntCancle = null;
+        /** @type ?HTMLElement */
+        this.textBox = null;
 
     }
 
@@ -53,46 +60,70 @@ export class Comment extends HTMLElement {
         this.contentDiv = document.createElement('div');
         this.appendChild(this.contentDiv);
 
+        this.textBox = document.createElement('input');
+        this.textBox.setAttribute('type', 'text');
+        this.textBox.style.display = 'none';
+        this.appendChild(this.textBox);
+
         this.bntModify = document.createElement('button');
         this.appendChild(this.bntModify);
 
         this.bntDelete = document.createElement('button');
         this.appendChild(this.bntDelete);
 
+        this.bntConfirm = document.createElement('button');
+        this.appendChild(this.bntConfirm);
+        this.bntConfirm.style.display = 'none';
+
+        this.bntCancle = document.createElement('button');
+        this.appendChild(this.bntCancle);
+        this.bntCancle.style.display = 'none';
+
+        // 수정 버튼
         this.bntModify.onclick = function () {
-            this.bntConfirm = document.createElement('button');
-            this.appendChild(this.bntConfirm);
+            this.parentElement.bntModify.style.display = 'none';
+            this.parentElement.bntDelete.style.display = 'none';
+            this.parentElement.contentDiv.style.display = 'none';
+            this.parentElement.bntConfirm.style.display = 'block';
+            this.parentElement.bntCancle.style.display = 'block';
+            this.parentElement.textBox.style.display = 'block';
 
-            const text = document.createElement('input');
-            text.type = 'text';
 
-            request('/comment/' + this.commentId, {
-                method: RequestMethod.PUT,
-                body: JSON.stringify(this.commentId)
-            }).then(response => {
-                switch (response.status) {
-                    case 200 :
-                        console.log('성공');
-                        break;
-                    case 400:
-                        console.log('실패');
-                        break;
-                    case 401:
-                        console.log('로그인이 필요합니다');
-                        alert('로그인이 필요합니다');
-                        // 로그인 페이지로 redirect
-                        redirect('/login');
-                        break;
-                    case 403:
-                        console.log('권한이 없습니다');
-                        alert('권한이 없습니다');
-                        break;
-                    default :
-                        console.log('예상치 목한 에러');
-                        break;
-                }
-            });
+            // 확인 버튼
+            this.parentElement.bntConfirm.onclick = function () {
+                request('/comment/' + this.parentElement.commentId, {
+                    method: RequestMethod.PUT,
+                    body: JSON.stringify(this.parentElement.commentId)
+                }).then(response => {
+                    switch (response.status) {
+                        case 200 :
+                            console.log('성공');
+                            break;
+                        case 400:
+                            console.log('실패');
+                            break;
+                        case 401:
+                            console.log('로그인이 필요합니다');
+                            alert('로그인이 필요합니다');
+                            // 로그인 페이지로 redirect
+                            redirect('/login');
+                            break;
+                        case 403:
+                            console.log('권한이 없습니다');
+                            alert('권한이 없습니다');
+                            break;
+                        default :
+                            console.log('예상치 목한 에러');
+                            break;
+                    }
+                });
+            }
+            // 취소 버튼
+            this.parentElement.bntCancle.onclick = function () {
+
+            }
         }
+        // 삭제 버튼
         this.bntDelete.onclick = function () {
             request('/comment/' + this.commentId, {
                 method: RequestMethod.DELETE,
@@ -128,8 +159,11 @@ export class Comment extends HTMLElement {
         this.authorDiv.innerText = this.nickname;
         this.timeDiv.innerText = millisToTimeString(this.regTime);
         this.contentDiv.innerText = this.content;
-        this.bntModify.innerHTML = '수정';
-        this.bntDelete.innerHTML = '삭제';
+        this.bntModify.innerText = '수정';
+        this.bntDelete.innerText = '삭제';
+        this.bntConfirm.innerText = '확인';
+        this.bntCancle.innerText = '취소';
+        this.textBox.setAttribute('value', this.content);
     }
 }
 
