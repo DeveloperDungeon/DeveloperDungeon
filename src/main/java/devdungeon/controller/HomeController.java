d48e699b2fbece9d30f90f373ff347b4d817ae00
2014004166 <2014004166@hanyang.ac.kr>
2020-08-18 14:30:56 +0900

    FIX: 챕터 임시 테스트

--- src/main/java/devdungeon/controller/HomeController.java
+++ src/main/java/devdungeon/controller/HomeController.java
@@ -2,6 +2,7 @@ package devdungeon.controller;
 
 import devdungeon.domain.ChapterVO;
 import devdungeon.domain.QuestVO;
+import devdungeon.mapper.ChapterMapper;
 import devdungeon.service.QuestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -17,25 +18,25 @@ public class HomeController {
 
     private final QuestService questService;
 
-//    private final ChapterMapper chapterMapper;
+    private final ChapterMapper chapterMapper;
 
     private int counter = 0;
 
-//
-//    @GetMapping("/test")
-//    public String test() {
-//        System.out.println("들어왔습니다! " + counter++);
-//        ChapterVO chapter = new ChapterVO();
-//        chapter.setTitle("새로운 챕터");
-//        chapter.setDescription("새로운챕터 설명해드릴게요.");
-//        chapterMapper.insertChapter(chapter);
-//        int id = chapter.getId();
-//        List<String> list = new ArrayList<>();
-//        list.add("asdf");
-//        list.add("1111");
-//        chapterMapper.insertWhitelist(list,id);
-//        return "index";
-//    }
+
+    @GetMapping("/test")
+    public String test() {
+        System.out.println("들어왔습니다! " + counter++);
+        ChapterVO chapter = new ChapterVO();
+        chapter.setTitle("새로운 챕터");
+        chapter.setDescription("새로운챕터 설명해드릴게요.");
+        chapterMapper.insertChapter(chapter);
+        int id = chapter.getId();
+        List<String> list = new ArrayList<>();
+        list.add("asdf");
+        list.add("1111");
+        chapterMapper.insertWhitelist(list,id);
+        return "index";
+    }
 
     @GetMapping("/")
     public String welcomeToMyHouse(Model model) {
