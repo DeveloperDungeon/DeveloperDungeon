1c1193a18afbaca9dedd0b8e4677781084e33051
2014004166 <2014004166@hanyang.ac.kr>
2020-08-18 14:29:09 +0900

    ADD: 챕터추가 및 챕터사용자 추가

--- src/main/java/devdungeon/mapper/ChapterMapper.java
+++ src/main/java/devdungeon/mapper/ChapterMapper.java
@@ -1,13 +1,13 @@
-//package devdungeon.mapper;
-//
-//import devdungeon.domain.ChapterVO;
-//import org.apache.ibatis.annotations.Mapper;
-//
-//import java.util.List;
-//
-//@Mapper
-//public interface ChapterMapper {
-//    int insertChapter(ChapterVO chapterVO);
-//
-//    int insertWhitelist(List<String> userIds, int chapterId);
-//}
+package devdungeon.mapper;
+
+import devdungeon.domain.ChapterVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ChapterMapper {
+    int insertChapter(ChapterVO chapterVO);
+
+    int insertWhitelist(List<String> userIds, int chapterId);
+}
