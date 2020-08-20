3e027adfdf2947e2b98cfd3c283eb2752684d171
2014004166 <2014004166@hanyang.ac.kr>
2020-08-20 22:26:02 +0900

    RFT: 불필요한 멤버변수 제거

--- src/main/java/devdungeon/domain/ChapterVO.java
+++ src/main/java/devdungeon/domain/ChapterVO.java
@@ -2,18 +2,9 @@ package devdungeon.domain;
 
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 public class ChapterVO {
     private int id;
     private String title;
     private String description;
-
-    private List<QuestVO> quests;
-    private List<String> users;
-
-    public ChapterVO() {
-
-    }
 }
