b275f64eb24ba9f054c63c1e319c628d5f044e9b
kbcbang <kbcbang@gmail.com>
2020-08-25 23:06:50 +0900

    FIX: writeable->writable 오타 수정

--- src/main/java/devdungeon/api/ChapterAPI.java
+++ src/main/java/devdungeon/api/ChapterAPI.java
@@ -4,7 +4,6 @@ import devdungeon.api.annotation.ApiCertifyAnnotation;
 import devdungeon.domain.ChapterVO;
 import devdungeon.service.ChapterService;
 import devdungeon.template.RedirectTemplate;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,13 +29,13 @@ public class ChapterAPI {
 
     @GetMapping
     @ApiCertifyAnnotation
-    public ResponseEntity<List<ChapterVO>> getWritableChapter(@RequestParam(value = "writeable", required=false, defaultValue = "false")
-                                                                          boolean writeable) {
-        if(writeable) {
+    public ResponseEntity<List<ChapterVO>> getWritableChapter(@RequestParam(value = "writeable", required = false, defaultValue = "false")
+                                                                      boolean writable) {
+        if (writable) {
             String userId = (String) session.getAttribute("user");
             List<ChapterVO> chapterList = chapterService.findWritableChapters(userId);
             return new ResponseEntity<>(chapterList, HttpStatus.OK);
         }
-        return new ResponseEntity<>(null,HttpStatus.OK);
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
