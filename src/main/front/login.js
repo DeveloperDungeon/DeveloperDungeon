669ad48936151eefb2fb9a12a29c32ea16bfc657
이영차 <kares64@gmail.com>
2020-09-03 13:06:07 +0900

    ADD: 프론트에서 로그인시 잘못된 입력 경고창 출력

--- src/main/front/login.js
+++ src/main/front/login.js
@@ -3,25 +3,39 @@ import {request, RequestMethod} from './common/request';
 window.addEventListener('load', () => {
     const button = document.getElementById('bntLogin');
     button.onclick = () => {
+        let errorMessage = '';
+        let isValid = true;
         const id = document.getElementById('inputId').value;
         const pw = document.getElementById('inputPw').value;
         const body = {
             id: id,
             password: pw
         };
+        if (id.length === 0) {
+            errorMessage += '아이디를 입력해 주세요\n';
+            isValid = false;
+        }
+        if (pw.length === 0) {
+            errorMessage += '비밀번호를 입력해 주세요\n';
+            isValid = false;
+        }
+        if (isValid) {
+            request('/login', {
+                method: RequestMethod.POST,
+                body: JSON.stringify(body)
+            }).then(response => {
+                if (response.status === 400)
+                    switch (response.body.errorCode) {
+                        case 1 :
+                            alert('아이디 또는 비밀번호를 입력하세요');
+                            break;
+                        case 2 :
+                            alert('아이디 또는 비밀번호가 틀렸습니다');
+                    }
+            });
+        } else {
+            alert(errorMessage);
+        }
 
-        request('/login', {
-            method: RequestMethod.POST,
-            body: JSON.stringify(body)
-        }).then(response => {
-            if (response.status === 400)
-                switch (response.body.errorCode) {
-                    case 1 :
-                        alert('아이디 또는 비밀번호를 입력하세요');
-                        break;
-                    case 2 :
-                        alert('아이디 또는 비밀번호가 틀렸습니다');
-                }
-        });
     }
 });
