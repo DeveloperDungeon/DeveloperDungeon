9b0580087d7dba3a2dcc9a2a95ab45e05e5e3ea1
이영차 <kares64@gmail.com>
2020-09-04 20:13:52 +0900

    RFT: break 추가

--- src/main/front/login.js
+++ src/main/front/login.js
@@ -31,6 +31,7 @@ window.addEventListener('load', () => {
                             break;
                         case 2 :
                             alert('아이디 또는 비밀번호가 틀렸습니다');
+                            break;
                     }
             });
         } else {
